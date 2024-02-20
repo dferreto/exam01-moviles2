9c7f7df3eb90c6b85d57caeb11060018d79cf831
dferreto <diferretomo@est.utn.ac.cr>
2024-02-19 18:48:53 -0600

    Finalización del banner y titulos

--- src/styles/style_01.js
+++ src/styles/style_01.js
@@ -2,32 +2,35 @@ import {StyleSheet} from 'react-native';
 
 		export const style_01 = StyleSheet.create({
 
-            container: {
-                flex: 1,
-                backgroundColor: '#fff',
-              },
-              header: {
-                alignItems: 'center',
-                padding: 20,
-              },
-              headerText: {
-                fontSize: 24,
-                fontWeight: 'bold',
-              },
-              listItem: {
-                flexDirection: 'row',
-                padding: 10,
-                alignItems: 'center',
-                borderBottomWidth: 1,
-                borderBottomColor: '#ccc',
-              },
-              pokemonImage: {
-                width: 50,
-                height: 50,
-                marginRight: 10,
-              },
-              pokemonName: {
-                fontSize: 18,
-              },
+      container: {
+        flex: 1,
+        backgroundColor: '#fff', // El color de fondo puede ser ajustado
+      },
+      header: {
+        alignItems: 'center',
+        padding: 20,
+      },
+      logoPM: {
+        height: 100, // Ajusta el tamaño según sea necesario
+        resizeMode: 'contain', // Esto asegura que el logo se ajuste al tamaño sin perder la proporción
+      },
+      title1: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 10, // Ajusta el espacio si es necesario
+        textAlign: 'center'
+      },
+      navBar: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        paddingVertical: 2, // Ajusta el espacio vertical si es necesario
+        backgroundColor: '#365DAA', // Ajusta el color de fondo si es necesario
+      },
+      navItem: {
+        fontSize: 14,
+        color: '#FFCB03',
+        justifyContent: 'flex-start'
+      },
+           
         });
 	
--- src/views/home.js
+++ src/views/home.js
@@ -1,15 +1,37 @@
 import React from 'react';
-		import {SafeAreaView, Text, View} from 'react-native';
+import { SafeAreaView, Text, View, Image, TouchableOpacity } from 'react-native';
 		import {style_01} from '../styles/style_01';
 
-		const Home = ({navigation}) => {
-		  return (
-			<SafeAreaView>
-			  <View>
-				<Text style={style_01.tit_01}>Express Notes</Text>
-			  </View>
-			</SafeAreaView>
-		  );
-		};
+		const Home = ({ navigation }) => {
+            return (
+              <SafeAreaView style={style_01.container}>
+                <View style={style_01.header}>
+                  <Image 
+                    source={require('../imgs/logoPM.png')} // Asegúrate de que la imagen está en la ruta correcta
+                    style={style_01.logoPM}
+                  />
+                </View>
+                <View style={style_01.navBar}>
+                  <TouchableOpacity onPress={() => navigation.navigate('First')}>
+                    <Text style={style_01.navItem}>First gen</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => navigation.navigate('Second')}>
+                    <Text style={style_01.navItem}>Second gen</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => navigation.navigate('Third')}>
+                    <Text style={style_01.navItem}>Third gen</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => navigation.navigate('Fourth')}>
+                    <Text style={style_01.navItem}>Fourth gen</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => navigation.navigate('Fifth')}>
+                    <Text style={style_01.navItem}>Fifth gen</Text>
+                  </TouchableOpacity>
+                </View>
+                <Text style={style_01.title1}>First Generation</Text>
+                {/* Aquí añadirías el resto del contenido de tu vista como la lista de Pokémons */}
+              </SafeAreaView>
+            );
+          };
 
 		export default Home;
